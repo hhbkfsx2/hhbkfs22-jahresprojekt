61e1a968d709eb8fbddf6712f99991ed1439f96f
Kilian <kilian.engelhardt@gmail.com>
2016-04-17 20:19:37 +0200

    ping-test.sh completed

--- scripts/8gruppe_scripts_ping-test.sh
+++ scripts/8gruppe_scripts_ping-test.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 # Dieses Skript testet die Erreichbarkeit der Server im Netzwerksegment per 'ping'
-# Webserver:	2001:4dd0:fc0b:0001::2
-# Mailserver:	2001:4dd0:fc0b:0002::2
+# Webserver:	2001:4dd0:fc0b::2
+# Mailserver:	2001:4dd0:fc0b::3
 
-IPADDR_WEB="197.0.0.1"
 #Prefix: /64
-#IPADDR_WEB="2001:4dd0:fc0b::2"
-#IPADDR_MAIL="2001:4dd0:fc0b::3"
+IPADDR_WEB="2001:4dd0:fc0b::2"
+IPADDR_MAIL="2001:4dd0:fc0b::3"
+#IPADDR_WEB="197.0.0.1"
 
 LOGFILE="ping-test.log"
 
 if [[ -e $(which ping) ]]; then
     echo "ping-Test $(date +%Y%m%d-%H:%M:%S)" >> ${LOGFILE}
     ping ${IPADDR_WEB} -c3 -W3  && echo "OK: Webserver (${IPADDR_WEB}) erreichbar." | tee -a ${LOGFILE} || echo "ERR: Webserver (${IPADDR_WEB}) nicht erreichbar." | tee -a ${LOGFILE}
-
-
-
+    ping ${IPADDR_MAIL} -c3 -W3  && echo "OK: Webserver (${IPADDR_MAIL}) erreichbar." | tee -a ${LOGFILE} || echo "ERR: Webserver (${IPADDR_MAIL}) nicht erreichbar." | tee -a ${LOGFILE}
 
 else
     echo "ERR: 'ping'-command nicht gefunden."
