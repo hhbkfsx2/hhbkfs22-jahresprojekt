45d2b61042bad97fde1718ca36b089fab46e93e9
Kilian <kilian.engelhardt@gmail.com>
2016-06-15 15:30:14 +0200

    Zeitplanung Arbeitspackete hinzugefügt

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Jahresprojekt der Klasse HHBK FS22 - Gruppe 8
 
 ### Pflichtenheft
 Änderungen sind in den jeweilgen Kapiteldateien zu machen. Anschließend wird per `generate_kapitel-tex.sh` die Datei `8gruppe_pflichtenheft_00_kapitel.tex` neu generiert, welche in der `..._00_main.tex` includet wird.
+
+### technische Dokumentation
--- scripts/8gruppe_scripts_ping-test.sh
+++ scripts/8gruppe_scripts_ping-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Dieses Skript testet die Erreichbarkeit der Server im Netzwerksegment per 'ping'
-# Webserver:	2001:4dd0:fc0b::2
-# Mailserver:	2001:4dd0:fc0b::3
+# Webserver:	2001:4dd0:fc0b:a:2
+# Mailserver:	2001:4dd0:fc0b:a:3
 
 #Prefix: /64
-IPADDR_WEB="2001:4dd0:fc0b::2"
-IPADDR_MAIL="2001:4dd0:fc0b::3"
+IPADDR_WEB="2001:4dd0:fc0b:a:2"
+IPADDR_MAIL="2001:4dd0:fc0b:a:3"
 #IPADDR_WEB="197.0.0.1"
 
 LOGFILE="ping-test.log"
