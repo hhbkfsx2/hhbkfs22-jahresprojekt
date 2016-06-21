1d38eff0edd68df273120105e188f97f7be42a5e
Kilian <kilian.engelhardt@gmail.com>
2016-06-21 08:50:10 +0200

    ip fixed

--- scripts/test-ipv6.sh
+++ scripts/test-ipv6.sh
@@ -6,11 +6,12 @@ RIP1="2001:4dd0:fc0b:a::1"
 RIP2="2001:4dd0:fc0b:f4::1"
 SIP1="2001:4dd0:fc0b:a::2"
 SIP2="2001:4dd0:fc0b:f4::2"
-KVM="2001:4dd0:fc0b:a::3"
+KVM1="2001:4dd0:fc0b:a::3"
+KVM2="2001:4dd0:fc0b:f4::3"
 SRV="2001:4dd0:fc0b:a::4"
-DC="2001:4dd0:fc0b:f4::6"
+DC="2001:4dd0:fc0b:f4::5"
 
-IP="${RIP1} ${RIP2} ${SIP1} ${SIP2} ${KVM} ${SRV} ${DC}"
+IP="${RIP1} ${RIP2} ${SIP1} ${SIP2} ${KVM1} ${KVM2} ${SRV} ${DC}"
 
 for i in ${IP}; do
 	echo -e "Ping-Test $(date +%Y%d%m):\n"
